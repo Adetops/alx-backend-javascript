97efe0abd830fefe96be33eaa0f710261ce2574c
Adetops <heritagediwamu@gmail.com>
2024-02-27 13:47:36 +0100

    basic execution

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Heritage Adeleke <https://github.com/Adetops>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
